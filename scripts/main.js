919bf681ad45de6cddd790814f5fdb04203f5a96
Kelsi <kelsistez@gmail.com>
2019-12-12 21:20:10 -0800

    logo

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,17 @@ $('.fas').click(function(){
 	$('.fas').toggleClass('fa-times');
 });
 
-var num = 300; //number of pixels before modifying styles
+var num = 350; //number of pixels before modifying styles
 
 $(window).bind('scroll', function () {
     if ($(window).scrollTop() > num) {
         $('.navbar').addClass('sticky');
         $('.main').addClass('margin-fix');
+        // $('.logo').slideDown('slow');
     } else {
         $('.navbar').removeClass('sticky');
         $('.main').removeClass('margin-fix');
+        // $('.logo').hide();
     }
 });
+
