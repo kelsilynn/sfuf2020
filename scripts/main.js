6670c2c2f6cda4100fe1732647907a11dad61c91
Kelsi <kelsistez@gmail.com>
2019-12-14 16:53:46 -0800

    nav logo

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,24 @@ $('.fas').click(function(){
 
 var num = 350; //number of pixels before modifying styles
 
+$(document).ready(function() {
+    if ($(window).scrollTop() > num) {
+        $('.navbar').addClass('sticky');
+        $('.main').addClass('margin-fix');
+        $('.top-nav').addClass('show-top-nav');
+    }
+});
+
 $(window).bind('scroll', function () {
     if ($(window).scrollTop() > num) {
         $('.navbar').addClass('sticky');
         $('.main').addClass('margin-fix');
-        // $('.logo').slideDown('slow');
+        // $('.top-nav').slideDown("slow")
+        $('.top-nav').addClass('show-top-nav');
     } else {
         $('.navbar').removeClass('sticky');
         $('.main').removeClass('margin-fix');
-        // $('.logo').hide();
+        $('.top-nav').removeClass('show-top-nav');
     }
 });
 
